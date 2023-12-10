40534c28d8ac3387eac11321dc564c3c3efe187a
dhirajsahh <dhirajmahato2019@gmail.com>
2023-12-10 20:12:24 +0545

    complete home and about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className=" px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-700"> About</h1>
+      <p>
+        This is a full stack web application built with the MERN .It includes
+        authentication featueres Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Laudantium voluptatibus corrupti eaque nam. Doloremque
+        quos et, dignissimos beatae corrupti quo. Repellat cumque recusandae
+        aspernatur nisi sint facilis a, consectetur laboriosam.
+      </p>
+    </div>
+  );
 };
 
 export default About;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className=" px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-700">
+        {" "}
+        Welcome to my Auth App
+      </h1>
+      <p>
+        This is a full stack web application built with the MERN .It includes
+        authentication featueres Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Laudantium voluptatibus corrupti eaque nam. Doloremque
+        quos et, dignissimos beatae corrupti quo. Repellat cumque recusandae
+        aspernatur nisi sint facilis a, consectetur laboriosam.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
