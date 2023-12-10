79bc77d23c0fc19140a08fdfb42f306add08a18d
dhirajsahh <dhirajmahato2019@gmail.com>
2023-12-10 20:33:25 +0545

    add pre production changes

--- api/index.js
+++ api/index.js
@@ -6,8 +6,16 @@ import authRoutes from "./routes/auth.route.js";
 import cors from "cors";
 
 import cookieParser from "cookie-parser";
-const app = express();
+import path from path ;
 connectdb();
+const __dirname=path.resolve();
+
+const app = express();
+app.use(express.static(path.join(__dirname,'/client/dist')))
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
+
 
 app.use(express.json());
 app.use(cors());
