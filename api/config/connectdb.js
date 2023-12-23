ed20ba3d7e56f122b75ae21b4d0eaed203ff1bad
dhirajsahh <dhirajmahato2019@gmail.com>
2023-12-23 08:25:44 +0545

    setup for deploy

--- api/config/connectdb.js
+++ api/config/connectdb.js
@@ -6,7 +6,6 @@ const connectdb = async () => {
   try {
     const connect = await mongoose.connect(process.env.MONGO_URL);
     console.log("Connected to MongoDB");
-    console.log(connect.connection.host);
   } catch (error) {
     console.log("Error occured during connecting mongodb", error);
   }
